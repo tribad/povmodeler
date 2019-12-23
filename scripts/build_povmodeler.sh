7c7ce499d3f1ef791045566d46ef112f8e611d38
Hans-Juergen Lange <hjl@simulated-universe.de>
2019-12-23 09:06:48 +0100

    Update to my buildscript

--- scripts/build_povmodeler.sh
+++ scripts/build_povmodeler.sh
@@ -15,31 +15,6 @@ else
     echo "Could not find os-release information"
 fi;
 ##
-## Check if QMAKE is set to some location
-if [ -z $QMAKE ] ; then
-    for QMAKE in /usr/local/bin /usr/bin ; do 
-        if [ -x ${QMAKE}/qmake ] ; then 
-            echo Checking $QMAKE for a usable qt version.
-            ##
-            ##  Get the qmake version info
-            QMAKE_RESULT=`${QMAKE}/qmake --version`
-            ##
-            ##  Get the version
-            export QT_VERSION=`echo $QMAKE_RESULT | awk '{print substr($0, match ($0, "[0-9]+[.][0-9]+[.][0-9]+"), RLENGTH)}'`
-            qt_version=( ${QT_VERSION//./ } )
-            export QT_VERSION_MAJOR="${qt_version[0]}"
-            export QT_VERSION_MINOR="${qt_version[1]}"
-            export QT_VERSION_PATCH="${qt_version[2]}"
-            ##
-            ##  Get the path. Be aware of the bunch of backslashes. They are because they get more and more reduced so at the end
-            ##  awk see's a \s
-            export QMAKE_PATH=`echo $QMAKE_RESULT | awk '{print substr($0, match ($0, "[0-9]+[.][0-9]+[.][0-9]+\\\\sin\\\\s")+RLENGTH)}'`
-            export QT_INCLUDE_DIR=${QMAKE_PATH}/../include
-            export QT_LIBRARY_DIR=${QMAKE_PATH}/../lib
-        fi;
-    done 
-fi;
-##
 ##  Checking the number of CPUs available
 CPUCOUNT=`cat /proc/cpuinfo | grep processor -c`
 ##
@@ -58,7 +33,7 @@ case $OSID in
         export OSPACKAGE=DEB
     ;;
     slackware)
-        export OSPACKAGE=NONE
+        export OSPACKAGE=SLACK
     ;;
     *)
         export OSPACKAGE=TGZ
@@ -68,33 +43,37 @@ echo "Building for "${OSID}"-"${OSVERSION}" a "${OSPACKAGE}" package"
 
 ##
 ## if  we are root expecting a clone there.
-if [ "$UID" = "0" ]; then
-    cd /usr/src/povmodeler
-fi;
+cd /tmp
 ##
 ##
 ##
 ##  Get the last updates
-git pull
+git clone https://github.com/tribad/povmodeler.git
+cd povmodeler
 ##
 ##  go to the requested branch
 BRANCH=${1:-original}
 git checkout $BRANCH
 ##
 ##  re-create build environment
-cmake .
-##
-##  cleanup old build
-make clean
+mkdir build
+cd build
+cmake ..
 ##
 ##  build
 make -j$((CPUCOUNT+1))
 ##
 ##  strip the symbols from the executable.
-strip povmodeler
+strip -s povmodeler
 ##
 ##  make the package
-if [ "$OSPACKAGE" != "NONE" ] ; then
+if [ "$OSPACKAGE" != "SLACK" ] ; then
     cpack -G ${OSPACKAGE}
+else
+    mkdir -p /tmp/build/install
+    make install DESTDIR=/tmp/build
+    cp ../slack-desc /tmp/build/install
+    cd /tmp/build/
+    makepkg -l y -c n ../povmodeler-${VERSION}-x86_64-${OSVERSION}.tgz
 fi;
 
