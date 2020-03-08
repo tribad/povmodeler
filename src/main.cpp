9b42d4a61a830b370bc1aaa1724a99da9f5c61fb
Hans-Juergen Lange <hjl@simulated-universe.de>
2020-03-08 14:23:42 +0100

    Now a version that builds. But only the empty main.

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+// *************************************************************************************************************
+//
+//  Modul-Name     : main.cpp
+//
+//  Copyrights by Hans-Juergen Lange <hjl@simulated-universe.de>. All rights reserved.
+//
+// *************************************************************************************************************
+#include "main.h"
+// Optional
+int main(int argc, const char** argv) {
+    int retval = 0;
+// User-Defined-Code:AAAAAAFwukSfIpDfer4=
+// End-Of-UDC:AAAAAAFwukSfIpDfer4=
+    return  (retval);
+}
+
