a713f7e60fbca60ab014c71a21a99f100ecb7aac
Hans-Juergen Lange <hjl@simulated-universe.de>
2020-03-10 12:53:18 +0100

    These are the new files. Some of them have been deleted some versions before. But now everything is fine. I think

---
+++ src/rendermgr/main.cpp
@@ -0,0 +1,16 @@
+// *************************************************************************************************************
+//
+//  Modul-Name     : main.cpp
+//
+//  Copyrights by Hans-Juergen Lange <hjl@simulated-universe.de>. All rights reserved.
+//
+// *************************************************************************************************************
+#include "main.h"
+// Optional
+int main(int argc, char** argv) {
+    int retval = 0;
+// User-Defined-Code:AAAAAAFwxCWKmA4fzBk=
+// End-Of-UDC:AAAAAAFwxCWKmA4fzBk=
+    return  (retval);
+}
+
---
+++ src/rendermgr/main.h
@@ -0,0 +1,14 @@
+// *************************************************************************************************************
+//
+//  Modul-Name     : main.h
+//
+//  Copyrights by Hans-Juergen Lange <hjl@simulated-universe.de>. All rights reserved.
+//
+// *************************************************************************************************************
+#pragma once
+#ifndef MAIN_INC
+#define MAIN_INC
+int main(int argc, char** argv) ;
+
+
+#endif  // MAIN_INC
