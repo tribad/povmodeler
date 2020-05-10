afb20ab99fef69329298435ec0c0c63ae5e4bbb0
Hans-Juergen Lange <hjl@simulated-universe.de>
2020-05-10 11:00:26 +0200

    Maybe I work on it later on.

---
+++ src/messages/eModelElementFormat.h
@@ -0,0 +1,18 @@
+// *************************************************************************************************************
+//
+//  Modul-Name     : eModelElementFormat.h
+//
+//  Copyrights by Hans-Juergen Lange <hjl@simulated-universe.de>. All rights reserved.
+//
+// *************************************************************************************************************
+#pragma once
+#ifndef EMODELELEMENTFORMAT_INC
+#define EMODELELEMENTFORMAT_INC
+//
+//                   E n u m e r a t i o n    d e c l a r a t i o n
+enum class eModelElementFormat {
+    Unknown,
+    KPM
+};
+
+#endif  // EMODELELEMENTFORMAT_INC
